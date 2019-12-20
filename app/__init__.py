fa095386126f9063c721555cf05d85f171173dfa
vladkex <kramar151997@yandex.ru>
2019-12-20 23:02:28 +0300

    я подключил к серверу постгресс хироку

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
-conn = psycopg2.connect(dbname='calculator_windows_and_doors', user='postgres', password='12345', host='localhost')
+conn = psycopg2.connect(dbname='d9tapmbgnodji3', user='myrprjlwezkjmr', password='d3a1dbd005ebdab311213e835004ddddaf4772c89b1bbf0cc3bfa9bd3bd44c79', host='ec2-174-129-255-37.compute-1.amazonaws.com')
 bootstrap = Bootstrap(app)
 
 from app import routes
